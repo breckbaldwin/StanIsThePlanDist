07948cf79247a8d505e3118a0716c8c96c73cb39
breckbaldwin <git@lingpipe.com>
2019-10-31 09:13:56 -0400

    cleaned up for pracice talk

--- puttBet.py
+++ puttBet.py
@@ -13,10 +13,12 @@ distance_of_putt = float(sys.argv[2])
 stan_program = CmdStanModel(stan_file=stan_program_path)
 stan_program.compile()
 
-fit = stan_program.sample(data= {"distance_of_putt":distance_of_putt}, csv_basename='./puttbet')
-console_output = open('puttbet-1.txt');
-print(console_output.read());
-#fit.get_drawset(params=[parameter_to_show]).hist(bins=50)
-fit.get_drawset(params=['prob_putt_sinks']).hist(bins=50,range=(0,1))
+json_data = data= {"distance_of_putt":distance_of_putt}
+
+fit = stan_program.sample(data=json_data,
+                          csv_basename='./puttbet')
+
+fit.get_drawset(params=['prob_putt_sinks']).hist(bins=50,
+                                                 range=(0,1))
 plt.show()
         
