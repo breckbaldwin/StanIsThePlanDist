3ad0ee1b1e283fdd4edaa7ff7d865e0ea7bc6bf1
breckbaldwin <git@lingpipe.com>
2019-11-05 09:08:23 -0500

    program to run met-hasting example, not working

---
+++ run_met_hastings.py
@@ -0,0 +1,15 @@
+import os
+from cmdstanpy import cmdstan_path, CmdStanModel
+import fileinput
+import sys
+
+stan_program = CmdStanModel(stan_file='stan/metropolis_one_param.stan')
+stan_program.compile()
+#stan_program.generate_quantities()
+#runs transformed data{}
+fit = stan_program.sample(csv_basename='./output',fixed_param=True,sampling_iters=0)
+
+#fit = stan_program.sample(csv_basename='./output')
+
+console_output = open('output-1.txt');
+print(console_output.read()); #good for catching errors and print statements in Stan prog        
